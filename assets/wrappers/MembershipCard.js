f7401f526ba6c2ebdc0c6573946bf4e48ee0174d
MrNobody <mrnobody@pop-os.localdomain>
2022-10-20 12:11:47 +0530

    learning git

--- assets/wrappers/MembershipCard.js
+++ assets/wrappers/MembershipCard.js
@@ -4,6 +4,7 @@ const Wrappers = styled.div`
   position: relative;
   background-color: white;
   box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.161);
+
   border-radius: 2rem;
   width: 300px;
   /* height: 300px; */
